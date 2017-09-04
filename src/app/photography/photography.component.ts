6335fb590d83093513d09fb6106ebd316b1866d4
George Saieed <saieed.george0@gmail.com>
2017-09-04 14:12:52 -0400

    update photography page

--- src/app/photography/photography.component.ts
+++ src/app/photography/photography.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HomeService } from './../services/home.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-photographyproj',
@@ -8,5 +10,17 @@ import { HomeService } from './../services/home.service';
 })
 export class PhotographyProjComponent {
 
+  public currProj: string;
+
+  constructor(private location: Location, private router: Router, private homeService: HomeService, private route: ActivatedRoute) {
+    router.events.subscribe((val) => {
+        window.scrollTo(0, 0);
+        this.currProj = location.path().slice(13);
+    });
+  }
+
+  public returnHome(): void {
+    this.router.navigate(['home', this.homeService.currentHomeState]);
+  }
 }
 
